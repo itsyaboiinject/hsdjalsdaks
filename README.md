259dcd979302c384d7b9f170fc50e9b578518a2f
6lasher <140984887+itsyaboiinject@users.noreply.github.com>
2023-10-09 17:02:03 -0400

    shit

---
+++ README.md
@@ -0,0 +1,25 @@
+# qProtect-Discord-Bot
+
+qProtect-Discord-Bot is a discord bot designed to obfuscate your jar files with qProtect Lite, a lightweight obfuscator for Java bytecode.
+
+## How to use
+
+https://youtu.be/xkB51NLx0Eo
+
+## Requirements
+
+To use qProtect-Discord-Bot, you will need Java 8 and Node v14 or higher installed on your system.
+
+## How to set up
+
+To set up qProtect-Discord-Bot, simply run the following commands in your terminal:
+```
+npm i
+node index.js
+```
+
+## Useful links
+
+- Bot Support: https://discord.gg/xfppERhApX
+- QProtect's Discord: https://discord.gg/PYwYGxVJcb
+- QProtect's Website: [https://mdma.dev/](https://mdma.dev/?referrer=N2E6R)
---
+++ index.js
@@ -0,0 +1,85 @@
+const { Client, GatewayIntentBits, Collection, MessageEmbed } = require(`discord.js`);
+const fs = require("fs");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ]
+});
+var { token, clientId } = require("./resources/config.json");
+const prompt = require('prompt-sync')();
+
+client.log = (message) => {
+    console.log(`[101Fuscator]: ${message}`);
+}
+
+/**
+ * Checks for a valid config containing both the token and client id
+ */
+
+if (!token || !clientId) {
+    client.log("Detected an invalid config file.")
+    client.log("Fixing ./resources/config.yml...")
+    const data = {
+        "token": "MTE2MTA0MjczNTQyNDM0NDE3NQ.GDHwG1.JkJZ1mjXRnZzhhpwzs6e7mDwwZDymEA9PRxjx8",
+        "clientId": "1161042735424344175"
+    };
+    fs.writeFileSync('./resources/config.json', JSON.stringify(data));
+    client.log("Fixed ./resources/config.yml")
+    return;
+}
+
+// Creating tmp folder if it doesnt exist.
+if (!fs.existsSync("./tmp")) {
+    client.log("Creating tmp folder...")
+    fs.mkdirSync("./tmp");
+    client.log("Created tmp folder!")
+}
+
+// If the token is misisng, it will request it
+if (token == "") {
+    client.log("Missing bot token. Please enter your bot's token.")
+    token = prompt("Token =>")
+    const data = {
+        "token": `${token}`,
+        "clientId": `${clientId}`
+    };
+
+    fs.writeFileSync('./resources/config.json', JSON.stringify(data));
+    client.log("Token has been set!")
+}
+
+// If the client is misisng, it will request it
+if (clientId == "") {
+    client.log("Missing client Id. Please enter your bot's client Id.")
+    clientId = prompt("Client ID =>")
+    const data = {
+        "token": `${token}`,
+        "clientId": `${clientId}`
+    };
+    fs.writeFileSync('./resources/config.json', JSON.stringify(data));
+    client.log("ClientId has been set!")
+}
+
+client.token = token;
+client.clientId = clientId;
+
+client.commands = new Collection();
+
+const functionFiles = fs.readdirSync("./functions/").filter(file => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync("./commands");
+
+
+(async () => {
+
+    for (const file of functionFiles) {
+        require(`./functions/${file}`)(client);
+    }
+
+    client.handleEvents(eventFiles, "./events");
+    client.handleCommands(commandFolders, "./commands");
+    await client.login(token);
+})();
